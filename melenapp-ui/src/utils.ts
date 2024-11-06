4c51956f2b2121ee3e56aeff22b218f11e540f22
bs <bstorniolo@gmail.com>
2024-11-06 14:55:30 +1100

    Fix video

--- melenapp-ui/src/utils.ts
+++ melenapp-ui/src/utils.ts
@@ -22,10 +22,62 @@ export const convertYoutubeUrl = (youtubeUrl:string) => {
       return `https://www.youtube.com/embed/${videoId}${params ? '?' + params : ''}`;
     }
 
+    const watchRegex = /https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)&?/;
+    const match2 = youtubeUrl.match(watchRegex);
+
+    if (match2 && match2[1]) {
+      const videoId = match2[1];
+      const params = new URLSearchParams(youtubeUrl.split('?')[1]); 
+
+      if (params.has('t')) {
+        const timeInSeconds = params.get('t');
+        params.delete('t'); // Remove 't' parameter
+        params.set('start', timeInSeconds || ''); // Add 'start' parameter with the same value
+      }
+
+      return `https://www.youtube.com/embed/${videoId}${params ? '?' + params : ''}`;
+    }
+
+
     if (youtubeUrl.includes('/short/'))
     {
       return youtubeUrl.replace('/short/', '/embed/')
     }
+
+    
+  
+    // // Check if the URL matches the 'watch' format
+    // if (watchRegex.test(youtubeUrl)) {
+      
+    //   return youtubeUrl.replace(
+    //     watchRegex,
+    //     'https://www.youtube.com/embed/$1?'
+    //   ); 
+    // }
     
     return youtubeUrl;
 };
+
+
+export const convertYoutubeWatchUrl = (youtubeUrl: string) => {
+  return youtubeUrl.replace(
+    /https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)&?/,
+    'https://www.youtube.com/embed/$1?'
+  );
+};
+
+
+//https://www.youtube.com/embed/gZcQNUSYrs8
+//https://www.youtube.com/embed/watch?v=gZcQNUSYrs8&t=17s
+
+
+//https://www.youtube.com/embed/gZcQNUSYrs8
+//https://www.youtube.com/embed/gZcQNUSYrs8?si=6pbQSsspccceQl0G
+//https://www.youtube.com/embed/gZcQNUSYrs8&t=17s&ab_channel=SIKANAEspa%C3%B1ol
+//https://www.youtube.com/embed/gZcQNUSYrs8?si=6pbQSsspccceQl0G
+
+
+
+
+//https://www.youtube.com/embed/gZcQNUSYrs8&t=17s&ab_channel=SIKANAEspa%C3%B1ol
+//https://www.youtube.com/watch?v=gZcQNUSYrs8&t=17s&ab_channel=SIKANAEspa%C3%B1ol
